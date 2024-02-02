6b9d36e12b454606190cc2b0f147430b4c55de41
DAEHO PARK <76561901+pdh4869@users.noreply.github.com>
2024-02-02 23:24:47 +0900

    [Bronze II] Title: 저항, Time: 44 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Bronze/1076. 저항/저항.py
@@ -0,0 +1,13 @@
+f = input()
+s = input()
+t = input()
+
+val = {"black":0, "brown": 1, "red":2, "orange":3,
+      "yellow":4, "green":5, "blue":6, "violet":7,
+      "grey":8, "white":9}
+
+mul = {"black":1, "brown": 10, "red":100, "orange":1000,
+      "yellow":10000, "green":100000, "blue":1000000, "violet":10000000,
+      "grey":100000000, "white":1000000000}
+
+print((val[f]*10 + val[s]) * mul[t])
