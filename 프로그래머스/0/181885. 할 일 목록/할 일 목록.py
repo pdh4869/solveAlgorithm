e2a024f7c98eb20fb8b616c18879f623d4c4cb71
DAEHO PARK <76561901+pdh4869@users.noreply.github.com>
2024-05-10 15:26:52 +0900

    [level 0] Title: 할 일 목록, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.py
@@ -0,0 +1,7 @@
+def solution(todo_list, finished):
+    answer = []
+    
+    for i in range(len(todo_list)):
+        if finished[i] == False:
+            answer.append(todo_list[i])
+    return answer
