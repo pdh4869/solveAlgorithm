3f7edc20b1e6a5645f1a5d337015a0e0769778fb
DAEHO PARK <76561901+pdh4869@users.noreply.github.com>
2024-05-10 22:35:12 +0900

    [level 0] Title: 수 조작하기 1, Time: 1.94 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.py
@@ -0,0 +1,15 @@
+def solution(n, control):
+    answer = 0
+    
+    for i in control:
+        if i == 'w':
+            n += 1
+        elif i == 'a':
+            n -= 10
+        elif i == 's':
+            n -= 1
+        elif i == 'd':
+            n += 10
+                
+    answer = n
+    return answer
