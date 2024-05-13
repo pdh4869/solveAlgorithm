db03369fdee9866d2ae0d3fc3f8540000a0258cc
DAEHO PARK <76561901+pdh4869@users.noreply.github.com>
2024-05-13 15:16:13 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.py
@@ -0,0 +1,14 @@
+def solution(n):
+    answer = []
+    
+    while n >= 1:
+        answer.append(n)
+        if n == 1:
+            break
+            
+        if n % 2 == 0:
+            n //= 2
+        else:
+            n = 3 * n + 1
+        
+    return answer
