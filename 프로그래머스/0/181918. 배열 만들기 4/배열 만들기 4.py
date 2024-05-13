8315e1dc98ead2fae2b39cb20548bd129d7fdc23
DAEHO PARK <76561901+pdh4869@users.noreply.github.com>
2024-05-13 15:42:50 +0900

    [level 0] Title: 배열 만들기 4, Time: 6.36 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.py
@@ -0,0 +1,17 @@
+from collections import deque
+
+def solution(arr):
+    stk = []
+    
+    i = 0
+    while i < len(arr):
+        if stk == []:
+            stk.append(arr[i])
+            i += 1
+        elif stk != [] and stk[-1] < arr[i]:
+            stk.append(arr[i])
+            i += 1
+        elif stk != [] and stk[-1] >= arr[i]:
+            stk.pop()
+            
+    return stk
