664e668b62675e00d002f12bd77d68bbec04571d
DAEHO PARK <76561901+pdh4869@users.noreply.github.com>
2024-05-26 23:16:08 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 67.35 ms, Memory: 26.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -0,0 +1,11 @@
+def solution(participant, completion):
+    answer = ''
+    
+    participant.sort()
+    completion.sort()
+
+    for i,j in zip(participant, completion):
+        if i != j:
+            return i
+
+    return participant[-1]
