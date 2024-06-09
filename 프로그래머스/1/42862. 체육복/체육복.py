267c8c5a3f44fad018546f43cf2a1aaac3c29561
DAEHO PARK <76561901+pdh4869@users.noreply.github.com>
2024-06-09 15:35:02 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -0,0 +1,16 @@
+def solution(n, lost, reserve):
+    lost.sort()
+    reserve.sort()
+    
+    for i in reserve[:]:
+        if i in lost:
+            lost.remove(i)
+            reserve.remove(i)
+            
+    for i in reserve:
+        if i-1 in lost:
+            lost.remove(i-1)
+        elif i+1 in lost:
+            lost.remove(i+1)
+            
+    return n - len(lost)
