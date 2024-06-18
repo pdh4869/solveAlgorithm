375f417ceb86cbe16c254ffa3601dd8392dcfe9b
DAEHO PARK <76561901+pdh4869@users.noreply.github.com>
2024-06-18 23:41:11 +0900

    [level 1] Title: 햄버거 만들기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.py
@@ -0,0 +1,11 @@
+def solution(ingredient):
+    answer = 0
+    tmp = []
+    for i in ingredient:
+        tmp.append(i)
+        if tmp[-4:] == [1,2,3,1]:
+            answer += 1
+            for _ in range(4):
+                tmp.pop()
+            
+    return answer
